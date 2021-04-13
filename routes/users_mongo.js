3286ba13190617026ccdefcd256bdb6031e5d0ef
qzcad <s.choporoff@gmail.com>
2021-04-13 13:08:07 +0300

    get method

--- routes/users_mongo.js
+++ routes/users_mongo.js
@@ -3,7 +3,15 @@ const jsonParser = express.json();
 var router = express.Router();
 var usersDb = require('../models/users')
 var md5 = require('md5');
-// var db = require('../database_mongo');
+
+
+function hash_pasword(body){
+  var b = body;
+  if ("password" in b){
+    b.password = md5(body.password)
+  }
+  return b;
+}
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
@@ -18,16 +26,9 @@ router.post('/', jsonParser, function (req, res){
     });
     return;
   }
-  var name = req.body.name;
-  var email = req.body.email;
-  var password = req.body.password;
-  var selector = {
-    name: name,
-    password: md5(password),
-    email: email
-  };
-  console.log(selector);
-  usersDb.insertOne(selector, function(err, result){
+  var body = hash_pasword(req.body);
+  console.log(body);
+  usersDb.create(body, function(err, result){
     if(err){
       res.status(400).json({
         "error": err.message
@@ -37,6 +38,69 @@ router.post('/', jsonParser, function (req, res){
     res.json({
       "message": "success",
       "data": 1
+    });
+  });
+});
+
+router.put('/:id', jsonParser, function (req, res) {
+  if (!req.body) {
+    res.status(400).json({
+      "error": "no request body"
+    });
+    return;
+  }
+  usersDb.findByIdAndUpdate(
+    req.params.id,
+    hash_pasword(req.body), 
+    {upsert: true},
+    function(err, doc) {
+      if(err){
+        res.status(400).json({
+          "error": err.message
+        });
+        return;
+      }
+      res.json({
+        "message": "success",
+        "data": 1
+      });
+    }
+  );
+});
+
+// router.get('/:id', jsonParser, function (req, res) {
+//   usersDb.findById(
+//     req.params.id,
+//     function (err, doc) {
+//       if(err){
+//         res.status(400).json({
+//           "error": err.message
+//         });
+//         return;
+//       }
+//       res.json(doc);
+//     }
+//   );
+// });
+
+// router.get('/:id', jsonParser, async function (req, res) {
+//   try{
+//     var doc = await usersDb.findById(req.params.id);
+//     res.json(doc);
+//   }
+//   catch(err){
+//     res.status(400).json({
+//       "error": err.message
+//     });
+//   }
+// });
+
+router.get('/:id', jsonParser, function (req, res) {
+  usersDb.findById(req.params.id).then((doc) => {
+    res.json(doc);
+  }).catch((err) => {
+    res.status(400).json({
+      "error": err.message
     });
   });
 });
